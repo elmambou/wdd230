cf6d9eba15f3f19ceff373398a07acb8185586d0
elmambou <77119578+elmambou@users.noreply.github.com>
2021-09-20 18:19:53 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,4 @@
-
+// set current year in footer
+//const currentDate = new Date();
+//document.querySelector('#year').textContent = currentDate.getFullYear();
+document.getElementById('year').innerHTML = (new Date).getFullYear();
