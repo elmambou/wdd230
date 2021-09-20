307fe890e551bf52b362cd59b38852e0245bf913
elmambou <77119578+elmambou@users.noreply.github.com>
2021-09-20 20:05:20 +0200

    Delete Week 01 directory

--- Week 01/main.js
+++
@@ -1,4 +0,0 @@
-// set current year in footer
-//const currentDate = new Date();
-//document.querySelector('#year').textContent = currentDate.getFullYear();
-document.getElementById('year').innerHTML = (new Date).getFullYear();
