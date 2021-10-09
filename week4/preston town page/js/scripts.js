2cb09c1d642fec9c11ffc8ded1143de4a2bbc4f7
elmambou <77119578+elmambou@users.noreply.github.com>
2021-10-09 21:37:17 +0200

    Update scripts.js

--- week4/preston town page/js/scripts.js
+++ week4/preston town page/js/scripts.js
@@ -1,1 +1,12 @@
+function toggleMenu() {
+    //console.log(document.getElementById("primaryNav").classList);
+    console.log(document.getElementById("primaryNav").classList.toggle("hide"));
+}
 
+const currentDate = {
+    day: 'numeric',
+    weekday: 'long',
+    month: 'long',
+    year: 'numeric',
+}
+document.getElementById("copyrightyear").textContent = new Date().toLocaleDateString('en-us', currentDate);
