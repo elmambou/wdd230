1d6cd6cf5f724d9c03f15c96eccf254faf7c80c9
elmambou <77119578+elmambou@users.noreply.github.com>
2021-10-17 05:03:35 +0200

    Update scripts.js

--- week5/preston_town_page/js/scripts.js
+++ week5/preston_town_page/js/scripts.js
@@ -1,1 +1,33 @@
+function toggleMenu() {
+    //console.log(document.getElementById("primaryNav").classList);
+    console.log(document.getElementById("primaryNav").classList.toggle("hide"));
+}
+
+const currentDate = {
+    day: 'numeric',
+    weekday: 'long',
+    month: 'long',
+    year: 'numeric',
+}
+document.getElementById("copyrightyear").textContent = new Date().toLocaleDateString('en-us', currentDate);
+
+
+const today = new Date();
+console.log(today);
+
+const dayNumber = today.getDay();
+console.log(dayNumber);
+
+const element = document.getElementById("message");
+
+if (dayNumber ==6) {
+   // console.log("Today is day 6");
+   element.classList.add("showme");
+
+} else {
+    //console.log("Today is day Not 6");
+   element.classList.add("hideme");
+
+}
+
 
