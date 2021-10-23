43bbfddf2ef361523669a5757c1d5884e03e5b72
elmambou <77119578+elmambou@users.noreply.github.com>
2021-10-24 01:46:32 +0200

    Update date.js

--- week6/js/date.js
+++ week6/js/date.js
@@ -1,1 +1,26 @@
+const date = new Date();
+
+const options = {
+  weekday: 'long',
+  year: 'numeric',
+  month: 'long',
+  day: 'numeric',
+};
+
+function bannerCalculator() {
+  const today = date.getDay();
+  const pancake = document.querySelector(".pancake");
+  
+  if (today===6) {
+     pancake.style.display='block';
+  }
+   else {
+      pancake.style.display= 'none';
+   }
+}
+bannerCalculator();
+
+
+
+
 
