367ce87bda8e22cd3c3fec4fb19edff3a18e33f5
elmambou <77119578+elmambou@users.noreply.github.com>
2021-10-24 00:44:00 +0200

    Update date.js

--- week5/preston_town_page/js/date.js
+++ week5/preston_town_page/js/date.js
@@ -7,26 +7,18 @@ const options = {
   day: 'numeric',
 };
 
-document.getElementById('currentdate');
-currentdate.textContent = date.toLocaleDateString('en-GB', options);
-
-document.getElementById('theYear');
-theYear.textContent = date.getFullYear();
-
-
 function bannerCalculator() {
-  const today = d.getDay();
+  const today = date.getDay();
   const pancake = document.querySelector(".pancake");
   
-  if (today = 3) {
-      pancake.style.display = 'block';
+  if (today===6) {
+     pancake.style.display='block';
   }
-  
-  else {
-      pancake.style.display = 'none';
-  }
+   else {
+      pancake.style.display= 'none';
+   }
 }
-
+bannerCalculator();
 
 
 
