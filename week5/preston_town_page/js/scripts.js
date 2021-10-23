5f1162313bee7e18a14aff7516177d66818affec
elmambou <77119578+elmambou@users.noreply.github.com>
2021-10-24 00:45:45 +0200

    Update scripts.js

--- week5/preston_town_page/js/scripts.js
+++ week5/preston_town_page/js/scripts.js
@@ -21,13 +21,16 @@ console.log(dayNumber);
 const element = document.getElementById("message");
 
 if (dayNumber ==6) {
-   // console.log("Today is day 6");
+   // console.log("Today is Saturday");
    element.classList.add("showme");
 
 } else {
-    //console.log("Today is day Not 6");
+    //console.log("Today is not Saturday 5");
    element.classList.add("hideme");
 
 }
+
+
+  
 
 
